f51c90ebfb8141d111df342fb06b60f4a7b2112e
A Lee <alee@fwparker.org>
2019-02-28 12:15:05 -0600

    Add files via upload

---
+++ Searching/ch15Lab.py
@@ -0,0 +1,6 @@
+'''
+Complete the chapter lab at http://programarcadegames.com/index.php?chapter=lab_spell_check
+'''
+
+
+
---
+++ Searching/search_problems.py
@@ -0,0 +1,28 @@
+'''
+Complete the following 3 searching problems using techniques
+from class and from Ch15 of the textbook website
+'''
+
+#1.  (7pts) Write code which finds and prints the longest
+# word in the provided dictionary.  If there are more
+# than one longest word, print them all.
+
+#2.  (10pts)  Write code which finds
+# The total word count AND average word length
+# in "AliceInWonderLand.txt"
+
+# CHOOSE ONE OF THE FOLLOWING TWO PROBLEMS
+
+#3 (13pts)  How many times does "Cheshire" occur in"AliceInWonderLand.txt"?
+# How many times does "Cat" occur?
+# How many times does "Cheshire" immediately followed by "Cat" occur?
+
+#### OR #####
+
+#3  (13pts)Find the most frequently occurring
+# seven letter word in "AliceInWonderLand.txt"
+
+# Challenge problem (for fun).  What words appear in the text of "Alice in Wonderland" that DO NOT occur in "Alice Through the Looking Glass".  Make a list.  You can substitute this for any of the above problems.
+
+
+
