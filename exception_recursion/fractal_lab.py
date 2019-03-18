7d415ca1d1a3db7764594f24be1c5db79dc80339
A Lee <alee@fwparker.org>
2019-03-17 22:04:34 -0500

    Create fractal_lab.py

---
+++ exception_recursion/fractal_lab.py
@@ -0,0 +1,23 @@
+
+#Turtle Recursion (25pts)
+
+#1)  Using the turtle library, create the H fractal pattern according to the file shown in the data folder. (10pts)  
+
+#2)  Using the turtle library, create any of the other recursive patterns in the data folder. 
+#  Challenge yourself to match your pattern as closely as you can to the image.  (10pts)
+#  Note:  The Koch snowflake shows step by step building of the fractal.  
+#  The rectangle fractal example shows 4 possible drawings of the same fractal (choose any one)
+
+
+#3)  Create your own work of recursive art with a repeating pattern of your making (or choose another one from the files).  
+#  It must be a repeated pattern using recursion (not just loops). Snowflakes, trees, and spirals are a common choice.  
+#  Play around and have fun with it.  (5pt) 
+
+#  General expectations for all problems
+#  Give all your fractals a depth of at least 4.  (Don't make programs that take forever though)  
+#  Ensure the recursive drawing is contained on the screen (at whatever size you set it).
+#  All three recursive drawings can be on the same program.  Just use screen.clear() between problems.  Alternately, you could draw to three different screen objects.
+#  Run your turtles at max speed.
+#  Have fun!
+ 
+#  Resource to help you out >>> https://docs.python.org/3.3/library/turtle
