e4535be292a7d11ef79d01b9db8eddf1f32ac01e
A Lee <alee@fwparker.org>
2019-03-25 16:18:53 -0500

    Create matplotlib_CTA_ridership.py

---
+++ matplotlib_CTA_ridership.py
@@ -0,0 +1,13 @@
+# CTA Ridership (28pts)
+
+#  Get the csv from the following data set.
+#  https://data.cityofchicago.org/Transportation/CTA-Ridership-Annual-Boarding-Totals/w8km-9pzd
+#  This shows CTA ridership by year going back to the 80s
+
+
+#1  Make a plot of rail usage for the most current 10 year period.  (year on x axis, and ridership on y) (5pts)
+#2  Plot bus usage for the same years as a second line on your graph. (5pts)
+#3  Plot bus and rail usage together on a third line on your graph. (5pts)
+#4  Add a title and label your axes. (5pts)
+#5  Add a legend to show data represented by each of the three lines. (5pts)
+#6  What trend or trends do you see in the data?  Offer at least two hypotheses which might explain the trend(s). (3pts)
