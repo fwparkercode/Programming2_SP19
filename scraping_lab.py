1a8738e995cf34e39032199d6918334a2fc83d0d
A Lee <alee@fwparker.org>
2019-04-03 13:54:08 -0500

    Update scraping_lab.py

--- scraping_lab.py
+++ scraping_lab.py
@@ -1,13 +1,16 @@
 # SCRAPING LAB 
 # (20pts)
 # Below is a link to a 10-day weather forecast at weather.com
-# Use urllib and BeautifulSoup to scrape data from the weather table.
-# Print a synopsis of the weather for the next 10 days.
-# Include the day and date, description, high and low temp, chance of rain, and wind. (2pts each)
-# Print the weather for each of the next 10 days to the user in a readable sentences.
+# Use requests and BeautifulSoup to scrape data from the weather table.
+# Print a synopsis of the weather for the next 10 days by grabbing each of the following:
+# Day, date, description, high and low temp, chance of rain, and wind. (2pts each)
+# Print the weather for each of the next 10 days to the user in readable sentences.
 # You can customize the text as you like, but it should be readable as a sentence without errors. (10pts)
 # You will need to target specific classes or other attributes to pull some parts of the data.
 # Sample sentence:  
 # Wednesday, April 4 will be Partly Cloudy/Windy with a High of 37 degrees and a low of 25, humidity at 52%.  There is 0% chance of rain with winds out of the WNW at 22 mph.
+
+# Note: Although it is possible to pull a description of the weather which includes much of this data, that is not the intent.
+# However, if you can do it and add the additional info, that works for me.
 
 url = "https://weather.com/weather/tenday/l/USIL0225:1:US"
