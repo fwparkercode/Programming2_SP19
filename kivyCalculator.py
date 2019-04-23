2dc257f0003fb28e9499141d50528045915d1436
A Lee <alee@fwparker.org>
2019-04-22 21:17:02 -0500

    Rename kivyCalc.py to kivyCalculator.py

---
+++ kivyCalculator.py
@@ -0,0 +1,22 @@
+# kivy Four Function calculator (35pts)
+# emulate the built in MACOS calculator app as best you can.
+
+# GENERAL LAYOUT (10pts)
+# Make a GridLayout with 6 rows
+# Place a text input for the display in the first row
+# Row 2 through 6 will be BoxLayouts which will contain buttons.
+
+FUNCTIONALITY (15pts)
+# When you press each button, the text is added to the display (see class example)
+# When you press the equal button, the displayed equation will be calculated and will now show the answer using the eval() function
+# When the equation cannot be evaluated, it will show "ERROR"
+# All buttons will be functional.
+
+
+# FORMATTING (10pts
+# Make it look and feel like the MACOS calculator as closely as possible.
+# Include spacing and padding as necessary within the grid.
+# Match the button colors as closely as you can (orange, gray, white etc)
+# Match the font and text color as closely as you can.
+# Match the size of the app and the relative button sizes as closely as you can.
+# Match the button layout as closely as you can.
