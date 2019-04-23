43bf054f8fbf653ca91f6d568ca6c1997e560ee4
A Lee <alee@fwparker.org>
2019-04-22 21:16:26 -0500

    Create kivyCalc.py

---
+++ kivyCalc.py
@@ -0,0 +1,25 @@
+# kivy Universal Gravity Calculator (30pts)
+
+# When we learned about exceptions, I asked you to make a Universal Gravity calculator
+
+# G is the universal gravity constant (6.674×10−11 N m^2 / kg^2)
+# m1 is the mass of object 1 in kg
+# m2 is the mass of object 2 in kg
+# r is the distance between the two objects in meters (center to center distance)
+# F is force in Newtons 
+# F = G * (m1 * m2) / r ** 2
+
+# Make a universal gravity calculator app using kivy
+# Your app will have the following:
+# -a label widget that shows the title of your app (2pts)
+# -a label widget and TextInput to input two masses and the radius (6pts)
+# -textInput widgets should not accept non-numerical input. (2pts)  
+# -a calculate button to execute the gravity calculation (3pts)
+# -answer label to show the calculation after you click on the button (3pts)
+# -when calculate button pressed, the answer appears in the answer label (5pts)
+# -value errors, and divide by zero errors do not occur (4pts)
+# -answer is formatted in scientific notation to two decimals (2pts)
+# -layout is formatted in a meaningful way to make application user friendly and attractive (3pts)   
+
+
+
